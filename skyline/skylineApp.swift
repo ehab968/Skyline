54148e5a10021f90b9cea6da86b2284176f924ae
Ehab Salah <e.salah82055@gmail.com>
2026-05-31 04:53:26 +0300

    Initial Commit

---
+++ skyline/skylineApp.swift
@@ -0,0 +1,17 @@
+//
+//  skylineApp.swift
+//  skyline
+//
+//  Created by Ehab Salah on 31/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct skylineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
